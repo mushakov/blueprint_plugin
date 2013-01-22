b09726b15b7ab333bd5fdc90f4b4b17f390313b4
dimtruck <dimalg@yahoo.com>
2013-01-22 07:21:29 +0000

    add knockoutjs

--- main.rb
+++ main.rb
@@ -4,52 +4,65 @@ require 'open-uri'
 require 'redis'
 require 'json'
 
-redis = Redis.new(:host => '198.61.212.105', :port => 6379)
-redis.del "blueprints"
-# get the data from glance and load into redis (if name doesn't match)
-# update the data in redis if name matches and data doesn't match
-doc = Nokogiri::HTML(open('https://blueprints.launchpad.net/glance'))
-doc.css('table#speclisting > tbody > tr').each do |row|
- blueprint = Hash.new
- # "priority"
- if row.elements[0].elements.count > 1 then
-   blueprint['priority'] = row.elements[0].elements[1].content
- elsif row.elements[0].elements.count > 0 then
-   blueprint['priority'] = row.elements[0].elements[0].content
+class GlanceParser
+ def initialize
+  redis = Redis.new(:host => '198.61.212.105', :port => 6379)
+  # get the data from glance and load into redis (if name doesn't match)
+  # update the data in redis if name matches and data doesn't match
+  doc = Nokogiri::HTML(open('https://blueprints.launchpad.net/glance'))
+  doc.css('table#speclisting > tbody > tr').each do |row|
+   blueprint = Hash.new
+   # "priority"
+   if row.elements[0].elements.count > 1 then
+     blueprint['priority'] = row.elements[0].elements[1].content
+   elsif row.elements[0].elements.count > 0 then
+     blueprint['priority'] = row.elements[0].elements[0].content
+   end
+   #'blueprint ==> '
+   if row.elements[1].elements.count > 1 then
+     blueprint['name'] = row.elements[1].elements[1].content
+   elsif row.elements[1].elements.count > 0 then
+     blueprint['name'] = row.elements[1].elements[0].content
+   end
+   # 'design ==> '
+   if row.elements[2].elements.count > 1 then
+     blueprint['design'] = row.elements[2].elements[1].content
+   elsif row.elements[2].elements.count > 0 then
+     blueprint['design'] = row.elements[2].elements[0].content
+   end
+   #'delivery ==> '
+   if row.elements[3].elements.count > 1 then
+     blueprint['delivery'] = row.elements[3].elements[1].content
+   elsif row.elements[3].elements.count > 0 then
+     blueprint['delivery'] = row.elements[3].elements[0].content
+   end
+   # 'assignee ==> '
+   if row.elements[4].elements.count > 1 then
+     blueprint['assignee'] = row.elements[4].elements[1].content
+   elsif row.elements[4].elements.count > 0 then
+     blueprint['assignee'] = row.elements[4].elements[0].content
+   end
+   # 'series ==> '
+   if row.elements[0].elements.count > 1 then
+     blueprint['series'] = row.elements[0].elements[1].content
+   elsif row.elements[0].elements.count > 0 then
+     blueprint['series'] = row.elements[0].elements[0].content
+   end
+  
+   blueprint_name = blueprint['name']
+   #check that name exists.  Retrieve it and update all values that come from glance.  Store it again in the database
+   #if it doesn't exist then store it directly
+   if redis.exists "blueprint___" + blueprint_name then
+    existing_blueprint = JSON.parse redis.get "blueprint___" + blueprint_name
+    existing_blueprint['design'] = blueprint['design']
+    existing_blueprint['delivery'] = blueprint['delivery']
+    existing_blueprint['assignee'] = blueprint['assignee']
+    existing_blueprint['series'] = blueprint['series']
+    redis.set "blueprint___" + blueprint_name, existing_blueprint.to_json
+   else
+    redis.set "blueprint___" + blueprint_name, blueprint.to_json
+    redis.rpush 'blueprints', blueprint_name
+   end
+  end
  end
- #'blueprint ==> '
- if row.elements[1].elements.count > 1 then
-   blueprint['name'] = row.elements[1].elements[1].content
- elsif row.elements[1].elements.count > 0 then
-   blueprint['name'] = row.elements[1].elements[0].content
- end
- # 'design ==> '
- if row.elements[2].elements.count > 1 then
-   blueprint['design'] = row.elements[2].elements[1].content
- elsif row.elements[2].elements.count > 0 then
-   blueprint['design'] = row.elements[2].elements[0].content
- end
- #'delivery ==> '
- if row.elements[3].elements.count > 1 then
-   blueprint['delivery'] = row.elements[3].elements[1].content
- elsif row.elements[3].elements.count > 0 then
-   blueprint['delivery'] = row.elements[3].elements[0].content
- end
- # 'assignee ==> '
- if row.elements[4].elements.count > 1 then
-   blueprint['assignee'] = row.elements[4].elements[1].content
- elsif row.elements[4].elements.count > 0 then
-   blueprint['assignee'] = row.elements[4].elements[0].content
- end
- # 'series ==> '
- if row.elements[0].elements.count > 1 then
-   blueprint['series'] = row.elements[0].elements[1].content
- elsif row.elements[0].elements.count > 0 then
-   blueprint['series'] = row.elements[0].elements[0].content
- end
- 
- blueprint_name = blueprint['name']
- redis.del "blueprint___" + blueprint_name
- redis.set "blueprint___" + blueprint_name, blueprint.to_json
- redis.rpush 'blueprints', blueprint_name
 end
--- show_data.rb
+++ show_data.rb
@@ -1,75 +1,122 @@
 require 'rubygems'
 require 'sinatra'
-#require 'slim'
 require 'json'
 require 'redis'
 require 'uri'
-
+require '/home/dimtruck/pet-projects/glance/main.rb'
 redis = Redis.new(:host => '198.61.212.105', :port => 6379)
  
 get '/' do
-  #slim :index
   status 200
+  html = "<html><head><title>Glance Rackspace</title><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\"><body>" 
+  html += "<button data-bind='click: refresh_data'>Refresh Data</button><br/>"
+  html += "<table><thead><tr><th>Blueprint</th><th>Priority</th><th>Delivery</th><th>Series</th><th>Assignee</th><th>Design</th><th>Is Racker</th><th>Comments</th><th>Actions</th></tr></thead><tbody data-bind='foreach: blueprints'>"
+  html += "<tr><td data-bind='text: name'></td>"
+  html += "<td data-bind='text: priority'></td>"
+  html += "<td data-bind='text: delivery'></td>"
+  html += "<td data-bind='text: series'></td>"
+  html += "<td data-bind='text: assignee'></td>"
+  html += "<td data-bind='text: design'></td>"
+  html += "<td><span data-bind='text: isracker,visible: showRacker'></span><input data-bind='value: isracker,visible: showRackerInput'/></td>"
+  html += "<td><span data-bind='text: comments,visible: showComments'></span><input data-bind='value: comments,visible: showCommentsInput'/></td>"
+  html += "<td><button data-bind='click: $parent.update, visible: showUpdateButton'>Update</button><button data-bind='click: $parent.save,visible: showSaveButton'>Save</button><button data-bind='click: $parent.cancel, visible: showCancelButton'>Cancel</button></td></tr>"
+  html += "</tbody></table></body>"
+  html += "<script src=\"http://code.jquery.com/jquery-1.8.2.min.js\"></script><script type=\"text/javascript\" src=\"http://ajax.aspnetcdn.com/ajax/knockout/knockout-2.2.1.js\"></script>"
+  html += "<script type=\"text/javascript\">"
+  html += "function Blueprint(item){"
+  html += "this.name = item.name; " 
+  html += "this.priority = item.priority; "
+  html += "this.delivery = item.delivery; "
+  html += "this.series = item.series; "
+  html += "this.assignee = item.assignee; "
+  html += "this.design = item.design; "
+  html += "this.isracker = ko.observable(item.isracker); "
+  html += "this.comments = ko.observable(item.comments); "
+  html += "this.showUpdateButton = ko.observable(true);"
+  html += "this.showSaveButton = ko.observable(false);"
+  html += "this.showCancelButton = ko.observable(false);"
+  html += "this.showRacker = ko.observable(true);"
+  html += "this.showRackerInput = ko.observable(false);"
+  html += "this.showComments = ko.observable(true);"
+  html += "this.showCommentsInput = ko.observable(false);"
+  html += "} "
+  html += "function BluePrintViewModel(){"
+  html += "var self = this;"
+  html += "self.blueprints = ko.observableArray([]);"
+  html += "$.getJSON('/load_blueprints', function(allData){"
+  html += "var mappedBlueprints = $.map(allData, function(item){ return new Blueprint(item); });"
+  html += "console.log(mappedBlueprints); "
+  html += "self.blueprints(mappedBlueprints);});"
+  html += "self.refresh_data = function(){"
+  html += "$.getJSON('/refresh_data',function(allData){"
+  html += "var mappedBlueprints = $.map(allData, function(item){ return new Blueprint(item); });"
+  html += "console.log(mappedBlueprints); "
+  html += "self.blueprints(mappedBlueprints);});"
+  html += "alert('updated');};"
+  html += "self.update = function(item){item.showRackerInput(true); item.showRacker(false); item.showComments(false); item.showCommentsInput(true); item.showUpdateButton(false); item.showSaveButton(true); item.showCancelButton(true);};"
+  html += "self.save = function(item){item.showCommentsInput(false); item.showRackerInput(false); item.showComments(true); item.showRacker(true); item.showUpdateButton(true); item.showSaveButton(false); item.showCancelButton(false);"
+  html += "console.log(ko.toJSON({item: item}));"
+  html += "$.ajax('/update_blueprint', {data: ko.toJSON({item:item}), type:'post',contentType:'application/json',success:function(data){}})};"
+  html += "self.cancel = function(item){item.showCommentsInput(false); item.showRackerInput(false); item.showComments(true); item.showRacker(true); item.showUpdateButton(true); item.showSaveButton(false); item.showCancelButton(false);};"
+  html += "} "
+  html += "$(document).ready(function(){ ko.applyBindings(new BluePrintViewModel())}); "
+  html += "</script></html>"
+end
+
+get '/load_blueprints' do
   count = redis.llen "blueprints"
   counter = 0
-  html = "<table><thead><tr><th>Blueprint</th><th>Priority</th><th>Delivery</th><th>Series</th><th>Assignee</th><th>Design</th></tr></thead><tbody>"
+  jsonArray = []
   while counter < count do
     blueprint = redis.lindex "blueprints", counter
     
     counter=counter+1
-    hash_blueprint = JSON.parse redis.get "blueprint___" + blueprint
-    priority = (hash_blueprint['priority'] if hash_blueprint.has_key?('priority')).to_s
-    delivery = (hash_blueprint['delivery'] if hash_blueprint.has_key?('delivery')).to_s
-    series = (hash_blueprint['series'] if hash_blueprint.has_key?('series')).to_s
-    assignee = (hash_blueprint['assignee'] if hash_blueprint.has_key?('assignee')).to_s 
-    design = (hash_blueprint['design'] if hash_blueprint.has_key?('design')).to_s
-    html += "<tr><td>" + hash_blueprint['name'] + "</td>"
-    html += "<td>" + priority + "</td>"
-    html += "<td>" + delivery + "</td>"
-    html += "<td>" + series  + "</td>"
-    html += "<td>" + assignee + "</td>"
-    html += "<td>" + design + "</td></tr>"
+    json = JSON.parse redis.get "blueprint___" + blueprint
+    jsonArray.push(json)
   end
-  html += "</tbody></table>"
-  #eval(redis.get "blueprints")
+  status 200
+  headers \
+    "content-type" => "application/json"
+  data =  JSON.generate jsonArray
 end
 
-get '/Question' do
-  questionId = 1;
- 
-  print questionId
+get '/refresh_data' do
+  glanceParser = GlanceParser.new
+  count = redis.llen "blueprints"
+  counter = 0
+  jsonArray = []
+  while counter < count do
+    blueprint = redis.lindex "blueprints", counter
+    
+    counter=counter+1
+    json = JSON.parse redis.get "blueprint___" + blueprint
+    jsonArray.push(json)
+  end
   status 200
-  content_type :json
-  question = eval(redis.get "questions:#{questionId}")
-  body question.to_json
+  headers \
+    "content-type" => "application/json"
+  data =  JSON.generate jsonArray   
 end
 
-get '/Question/:answerId' do
-  puts params[:answerId]
-  questionId = params[:answerId].to_i
-  question = eval(redis.get "questions:#{questionId}")
-  question[:EncodedQuestion] = URI.escape(question[:Question])
-  status 200
-  content_type :json
-  body question.to_json unless question.empty?
-end
+post '/update_blueprint' do
+ request.body.rewind
+ data = request.body.read
+ new_blueprint = JSON.parse data
+ #check the data 
+ blueprint = JSON.parse redis.get "blueprint___" + new_blueprint['item']['name']
+ if redis.exists "blueprint___" + new_blueprint['item']['name'] then
+    existing_blueprint = JSON.parse redis.get "blueprint___" + new_blueprint['item']['name']
+    existing_blueprint['design'] = new_blueprint['item']['design']
+    existing_blueprint['delivery'] = new_blueprint['item']['delivery']
+    existing_blueprint['assignee'] = new_blueprint['item']['assignee']
+    existing_blueprint['series'] = new_blueprint['item']['series']
+    if new_blueprint['item'].has_key?('comments') then
+      existing_blueprint['comments'] = new_blueprint['item']['comments']
+    end
+    if new_blueprint['item'].has_key?('isracker') then
+      existing_blueprint['isracker'] = new_blueprint['item']['isracker']
+    end
 
-put '/Answer/:questionId' do
-  # 1. check if the answer is correct for the question
-  # 2. get the translation of the text and whether this was a valid response
-  # 3. return the next question id, translation and text
-  questionId = params[:questionId].to_i
-  follow_ups = redis.get "follow_ups:#{questionId}"
-  follow_ups = follow_ups.split(',') { |s| s.to_i}
-  puts follow_ups
-  questionId = follow_ups.sample
-  data = JSON.parse request.body.read
-  resp = Hash.new
-  resp[:Id] = 1
-  resp[:Answer] = data['Answer']
-  resp[:Translation] = "does not exist yet."
-  resp[:Language] = "es"
-  resp[:QuestionId] = questionId
-  status 200
-  content_type :json
+    redis.set "blueprint___" + new_blueprint['item']['name'], existing_blueprint.to_json
+ end
 end
